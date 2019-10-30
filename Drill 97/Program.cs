8885ec906501ad49cca5e6cb3dbd1b73fb2fa32d
Alex Bainter <shreder108@gmail.com>
2019-10-30 11:49:34 -0600

    Step 97
    A simple string concatenation program
    with stringbuilder practice

---
+++ Drill 97/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "alex";
+            name = name.ToUpper();
+            string greeting = "Hello!";
+            string day = " I wish it wasn't snowing so ****ing much";
+            string sentence = (greeting + name + day);
+            Console.WriteLine(sentence);
+            StringBuilder paragraph = new StringBuilder();
+            paragraph.Append("But NOOOO");
+            
+            paragraph.Append("\nit just keeps snowing");
+            
+            paragraph.Append("\nand getting colder");
+            
+            paragraph.Append("\nand it shuts my power off every so often.");
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+
+        }
+    }
+}
