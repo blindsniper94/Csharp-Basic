c5771e75ae344108d0ad4712df5665cb76e6680e
Alex Bainter <shreder108@gmail.com>
2019-11-07 12:59:04 -0700

    C# Functions Practice

---
+++ Drill 113/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_113
+{
+    public class ClassOfNumbers
+    {
+        public int NumeroUno { get; set; }
+        
+        public static void TwoNumbersToTango(ClassOfNumbers Numero1, ClassOfNumbers Numero2)
+        {
+            Console.WriteLine("This function is inside a class");
+            int NumeroTres = Numero1.NumeroUno + Numero2.NumeroUno;
+            Console.WriteLine(NumeroTres);
+        }
+    }
+}
---
+++ Drill 113/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassOfNumbers Numero1 = new ClassOfNumbers();
+            ClassOfNumbers Numero2 = new ClassOfNumbers();
+            Console.WriteLine("Please give me a number");
+            Numero1.NumeroUno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please give me another number");
+            Numero2.NumeroUno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You choose numbers: "+ Numero1.NumeroUno +" and "+ Numero2.NumeroUno);
+            Console.ReadLine();
+            ClassOfNumbers.TwoNumbersToTango(Numero1: Numero1, Numero2: Numero2);
+            Console.WriteLine(Numero2.NumeroUno);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
