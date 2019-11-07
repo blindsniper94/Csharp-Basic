63bcd35a7081133ae4cf323a5a43ecfc5e5a6824
Alex Bainter <shreder108@gmail.com>
2019-11-07 13:58:24 -0700

    Drill 114
    Overloading Functions

---
+++ Drill 114/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_114
+{
+    class Class1
+    {
+       public int NumeroUno { get; set; }
+       public double NumeroDos { get; set; }
+       public string numSpelledOut { get; set; }
+
+
+        public static int functionUno(Class1 thisNum)
+        {
+
+            int answerUno = thisNum.NumeroUno * 2;
+            Console.WriteLine(answerUno);
+            Console.ReadLine();
+            return(answerUno);
+        }
+        public static decimal functionUno(Class1 thisNum,double extraNum = 10.5)
+        {
+            double answerDos = thisNum.NumeroUno / thisNum.NumeroDos + extraNum;
+            Console.WriteLine(answerDos);
+            Console.ReadLine();
+            return (Convert.ToDecimal(answerDos));
+        }
+        public static void functionUno(Class1 thisNum, int specialNum =5)
+        {
+            int spelledOut = Convert.ToInt32(thisNum.numSpelledOut);
+            int answerTres = spelledOut + specialNum;
+            Console.WriteLine(answerTres);
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ Drill 114/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_114
+{
+    class Program
+    {
+        int num1 = 17;
+        static void Main(string[] args)
+        {
+            Class1 thisNum = new Class1();
+            thisNum.NumeroDos = 2.5;
+            thisNum.NumeroUno = 12;
+            thisNum.numSpelledOut = "8";
+            Class1.functionUno(thisNum);
+            Class1.functionUno(thisNum,10);
+            Class1.functionUno(thisNum,18);
+            Console.ReadLine();
+            
+        }
+    }
+}
