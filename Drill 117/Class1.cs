c578884a66a9f75bd2c76a14e19a7349d26a85b3
Alex Bainter <shreder108@gmail.com>
2019-11-08 12:42:04 -0700

    Drill 117
    Overloading and output parameters practice

---
+++ Drill 117/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_117
+{
+    public class Class1
+    {
+        public int stuff { get; set; }
+        double result;
+        
+        public static void TwoNumbahs(Class1 NumeroUno, out double result)
+        {
+            Console.WriteLine("Your number divided by 2 is: ");
+            result = NumeroUno.stuff / 2;
+            Console.WriteLine(result); 
+        }
+        public static void TwoNumbahs(Class1 NumeroUno,Class1 NumeroDos, out double result)
+        {
+            Console.WriteLine("Adding Your numbers and dividing them by 2 is: ");
+            result = (NumeroUno.stuff+ NumeroDos.stuff) / 2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ Drill 117/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Class1 NumberoUno = new Class1();
+            Class1 NumberoDos = new Class1();
+            string optional = "";
+            Console.WriteLine("Please give me a number");
+            NumberoUno.stuff = Convert.ToInt32(Console.ReadLine());
+            Class1.TwoNumbahs(NumberoUno, out double result);
+            Console.ReadLine();
+            Console.WriteLine("Please give me another number!");
+            optional = Console.ReadLine();
+            if (string.IsNullOrEmpty(optional))
+            {
+                Class1.TwoNumbahs(NumberoUno,out result);
+            }
+            else
+            {
+                NumberoDos.stuff = Convert.ToInt32(optional);
+                Class1.TwoNumbahs(NumberoUno, NumberoDos, out result);
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
