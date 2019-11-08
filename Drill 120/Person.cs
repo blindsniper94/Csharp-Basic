d9f9270daa2a1a3a16765e7c86eb23c7fd4e63f5
Alex Bainter <shreder108@gmail.com>
2019-11-08 13:51:02 -0700

    Drill 120
    Class inheritance practice

---
+++ Drill 120/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_120
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        
+        public void sayName()
+        {
+            string writeName = firstName + " " + lastName;
+            Console.WriteLine(writeName);
+        }
+    }
+}
---
+++ Drill 120/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.firstName = "Alex";
+            person1.lastName = "Nunyabizness";
+            Emplyee newEmp = new Emplyee();
+            newEmp.firstName = "Sample";
+            newEmp.lastName = "Student";
+            person1.sayName();
+            newEmp.sayName();
+            Console.ReadLine();
+        }
+    }
+}
