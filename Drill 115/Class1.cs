61e4c402e4141982e37900df928ea613f50984a3
Alex Bainter <shreder108@gmail.com>
2019-11-08 10:07:32 -0700

    Drill 115
    Overloading functions practice

---
+++ Drill 115/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_115
+{
+    public class Class1
+    {
+        public int numbah { get; set; }
+
+        public static void OptionalParam(Class1 num1)
+        {
+            double answer = num1.numbah + 10;
+            Console.WriteLine(answer);
+            Console.ReadLine();
+            
+        }
+        public static void OptionalParam(Class1 num1,Class1 num2 )
+        {
+            double answer = num1.numbah + num2.numbah + 10;
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Drill 115/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 num1 = new Class1();
+            Class1 num2 = new Class1();
+            string optional = "";
+            
+            Console.WriteLine("Please give me a number.");
+            num1.numbah = Convert.ToInt32(Console.ReadLine());          
+            Console.WriteLine("Please give me another number.");
+            optional = Console.ReadLine();
+            if (string.IsNullOrEmpty(optional))
+            {
+                
+                Class1.OptionalParam(num1);
+            }
+            else
+            {
+                
+                num2.numbah = Convert.ToInt32(optional);
+                Class1.OptionalParam(num1, num2);
+            }
+            
+
+                      
+            
+        }
+    }
+}
