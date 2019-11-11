acd0b2eff05e83ef449aa360f2d80844bdc663bd
Alex Bainter <shreder108@gmail.com>
2019-11-11 13:17:53 -0700

    Drill 129
    Overloading operators

---
+++ Drill 129/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_126
+{
+    public class Employee:Person,IQuittable
+    {
+        
+        public override void SayName()
+        {
+            Console.WriteLine("First and Last Names");
+            base.SayName();
+            Console.ReadLine();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Woohoo It works!");
+        }
+        public static Employee operator ==(Employee newEmployee, Employee newPerson)
+        {
+
+            bool IdMatch = newEmployee.Id.Equals(newPerson.Id);
+            Console.WriteLine("Do these Id's match?" + IdMatch);
+            return newEmployee;
+            
+        }
+        public static Employee operator !=(Employee newEmployee, Employee newPerson)
+        {
+
+            bool IdMatch = newEmployee.Id.Equals(newPerson.Id);
+            Console.WriteLine("Do these Id's match?" + IdMatch);
+            return newEmployee;
+        }
+
+    }
+}
---
+++ Drill 129/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee()
+            {
+                Id = 1,
+                firstName = "Sample",               
+                lastName = "Student"
+            };
+            Employee newPerson = new Employee()
+            {
+                Id = 1,
+                firstName = "Stevin",
+                lastName = "Stevinson"
+            };
+            IQuittable quittable = new Employee();
+
+            newEmployee = newEmployee == newPerson;
+            newEmployee.SayName();
+            quittable.Quit();
+            Console.ReadLine();
+        }
+    }
+}
