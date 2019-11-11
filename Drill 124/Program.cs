101919570bf68de5819109b040023e2cf7f3ece2
Alex Bainter <shreder108@gmail.com>
2019-11-11 11:00:00 -0700

    Drill 124
    Practice with abstract classes and methods

---
+++ Drill 124/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            newEmployee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
