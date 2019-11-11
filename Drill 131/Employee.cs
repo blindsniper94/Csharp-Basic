77dec86b3b2731cda3aa453c7e89b6480d12b0a0
Alex Bainter <shreder108@gmail.com>
2019-11-11 14:50:40 -0700

    Drill 131
    Generic Practice

---
+++ Drill 131/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_131
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        public List<T> things { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("First and Last Names");
+            base.SayName();
+            Console.ReadLine();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Woohoo It works!");
+        }
+        //public static Employee operator ==(Employee newEmployee, Employee newPerson)
+        //{
+
+        //    bool IdMatch = newEmployee.Id.Equals(newPerson.Id);
+        //    Console.WriteLine("Do these Id's match?" + IdMatch);
+        //    return newEmployee;
+
+        //}
+        //public static Employee operator !=(Employee newEmployee, Employee newPerson)
+        //{
+
+        //    bool IdMatch = newEmployee.Id.Equals(newPerson.Id);
+        //    Console.WriteLine("Do these Id's match?" + IdMatch);
+        //    return newEmployee;
+        //}
+    }
+}
---
+++ Drill 131/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Drill_131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeeList = new Employee<string>();
+            employeeList.things = new List<string>() { "Item1", "Item2", "Item3" };
+            
+            Employee<int> employeeList2 = new Employee<int>();
+            employeeList2.things = new List<int>() { 1, 2, 3 };
+            
+            //Employee newEmployee = new Employee()
+            //{
+            //    Id = 1,
+            //    firstName = "Sample",
+            //    lastName = "Student"
+            //};
+            //Employee newPerson = new Employee()
+            //{
+            //    Id = 1,
+            //    firstName = "Stevin",
+            //    lastName = "Stevinson"
+            //};
+            //IQuittable quittable = new Employee();
+
+            //newEmployee = newEmployee == newPerson;
+            //newEmployee.SayName();
+            //quittable.Quit();
+            //Console.ReadLine();
+            foreach (string i in employeeList.things)
+            {
+                Console.WriteLine(i);
+                
+            };
+            foreach (int i in employeeList2.things)
+            {
+                Console.WriteLine(i);
+                
+            };
+            Console.ReadLine();
+        }
+    }
+}
