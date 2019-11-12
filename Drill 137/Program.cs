76be49692b6ac0b5282547b98e309fb40caaf9f7
Alex Bainter <shreder108@gmail.com>
2019-11-12 11:21:02 -0700

    Drill 137
    Practice with Structs

---
+++ Drill 137/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number Numero = new Number();
+            Numero.Amount = 17.72m;
+            Console.WriteLine(Numero.Amount);
+            Console.ReadLine();
+        }
+    }
+}
