f5fd93752c35c40b9df97e32a5a3a53a93ceaac9
Alex Bainter <shreder108@gmail.com>
2019-11-12 10:36:23 -0700

    Drill 134
    Enums Practice

---
+++ Drill 134/Days.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_134
+{
+    class Days
+    {
+        public DaysoftheWeek DaysoftheWeek { get; set; }
+    }
+    public enum DaysoftheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursaday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Drill 134/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Days day = new Days();
+                Console.WriteLine("What day of the week is it? In case you forgot The days are:");
+                foreach (string DayName in Enum.GetNames(typeof(DaysoftheWeek)))
+                {
+                    Console.WriteLine(DayName);
+
+                }
+                string UserDay = Console.ReadLine();
+                Enum.Parse(typeof(DaysoftheWeek), UserDay);
+
+
+                Console.WriteLine("So it's " + UserDay);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Sorry I didn't understand that. Please try again");
+                Console.ReadLine();
+            }
+        }
+    }
+    
+}
