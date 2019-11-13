9315f55e93dc19a92388a516f0dbc23e1774cd68
Alex Bainter <shreder108@gmail.com>
2019-11-13 14:14:16 -0700

    Drill 156
    Practice with DateTime

---
+++ Drill 156/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime =  DateTime.Now;
+            Console.WriteLine(currentDateTime);
+            Console.WriteLine("Please give me a number");
+            Double userNum = Convert.ToDouble(Console.ReadLine());
+            DateTime difference = currentDateTime.AddHours(userNum);
+            Console.WriteLine("In {0} hours it will be {1}", userNum, difference);
+            Console.ReadLine();
+        }
+    }
+}
