a4bee6dda19c59a1db6a0626080a17d5b27a7794
Alex Bainter <shreder108@gmail.com>
2019-11-13 13:31:09 -0700

    Drill 154
    File logging and File reading practice

---
+++ Drill 154/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Drill_154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number!");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            using(StreamWriter file = new StreamWriter(@"C:\Users\Alex\Documents\TechAcademyNotes\C#\log2.txt", true))
+            {
+                file.WriteLine(userNum);
+            }
+            string text = File.ReadAllText(@"C:\Users\Alex\Documents\TechAcademyNotes\C#\log2.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
