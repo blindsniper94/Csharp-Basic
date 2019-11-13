10d9d5a14a08820b46633f4b75d4be83d6cc1c84
Alex Bainter <shreder108@gmail.com>
2019-11-13 15:57:58 -0700

    Drill 162
    Const, var, and Chaining constructors practice

---
+++ Drill 162/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Alex";
+            var Dictionary = new List<string>();
+            practiceClass obj = new practiceClass();
+            Console.WriteLine("Hello {0}", name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Drill 162/practiceClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_162
+{
+    public class practiceClass
+    {
+        public practiceClass():this(10)
+        {
+
+        }
+        public practiceClass(int Age)
+        {
+
+        }
+    }
+}
